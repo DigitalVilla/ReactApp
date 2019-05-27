7e0f624e0304366a0506506887d2ecfe60193545
Digital Villa <digitalvilla01@gmail.com>
2019-05-26 20:53:43 -0700

    Basic layout for a new React app

---
+++ src/app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../sass/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="title-main">
+          Create The Unthinkable!
+        </h1>
+
+    </div>
+  );
+}
+
+export default App;
